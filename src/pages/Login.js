c96545f5b74cac76ce4de595d6394df78e4e1838
Milena <125682053+MilenaBasso@users.noreply.github.com>
2024-03-08 10:08:45 -0300

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,10 @@
 import { Text, View, StyleSheet, Image } from 'react-native';
 import { styles } from '../styles/StyleSheet';
 
-
-
-
 export default function Login() {
 
 return (
-  
 <View>
-
-
 </View>
 );
 }
