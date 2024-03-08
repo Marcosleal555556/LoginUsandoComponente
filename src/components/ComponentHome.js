85301975911626c05a81cca13f67a2b10b2fc782
Marcosleal555556 <marcosantoniocordeiro27@gmail.com>
2024-03-08 08:02:42 -0300

    up

--- src/components/ComponentHome.js
+++ src/components/ComponentHome.js
@@ -1,18 +1,22 @@
 import { Text, View, StyleSheet, ImageBackground } from 'react-native';
 import { styles } from '../styles/StyleSheet';
-const ImagemFundo = {
-  uri: 'https://img.freepik.com/vetores-gratis/fundo-branco-monocromatico-de-estilo-de-papel_23-2149008530.jpg?size=626&ext=jpg&ga=GA1.1.1803636316.1700611200&semt=ais',
+
+
+const Fundo = {
+  uri: 'https://img.freepik.com/fotos-gratis/fundo-preto-abstrato-da-grade-digital_53876-97647.jpg?size=626&ext=jpg&ga=GA1.1.1395880969.1709769600&semt=ais',
 }
-export default function Home({ source }){
+
+export default function Home() {
 
 return (
-<View style={styles.container}>
+  
+<View>
+<ImageBackground source={Fundo} resizeMode="cover" style={styles.fundo}>
 
+<Text>
+HOMEPAGE
 
-<ImageBackground source={require('../assets/imagens/imagem.png')} style={styles.image}>
-
-<Text style={styles.text}>Página Home</Text>
-
+</Text>
 
 </ImageBackground>
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,7 @@
 import { Text, View, StyleSheet } from 'react-native';
 import { styles } from '../styles/StyleSheet';
-import Home from '../components/ComponentHome';
+import Home from '../components/LoginImageBackgroundHome';
 import { ImageBackground, Text } from 'react-native-web';
 
 
 
- 
