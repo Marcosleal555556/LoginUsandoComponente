eea74f325d2173e62dc4193dffcf16937ab67441
Marcosleal555556 <marcosantoniocordeiro27@gmail.com>
2024-03-08 08:14:47 -0300

    up

--- src/components/ComponentAbout.js
+++ src/components/ComponentAbout.js
@@ -1,2 +1,25 @@
-import { Image, Text, View } from "react-native-web";
-import 
+import { Text, View, StyleSheet, Image } from 'react-native';
+import { styles } from '../styles/StyleSheet';
+
+
+const imagem = {
+  uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvUP0eZQkYRyfLZ3XfH8nWN-880jbLF8fUZAoAH_b4fg&s',
+}
+
+export default function Home() {
+
+return (
+  
+<View>
+<Image source={imagem} resizeMode="cover" style={styles.imagem}>
+
+<Text>
+Exemplo de página About
+
+</Text>
+
+</Image>
+
+</View>
+);
+}
