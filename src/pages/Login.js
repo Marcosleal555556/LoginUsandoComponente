b20a34e6e0f7af7a1914049394ba4dad5d857683
Marcosleal555556 <marcosantoniocordeiro27@gmail.com>
2024-03-08 13:39:21 -0300

    up

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,10 @@ import { View } from 'react-native';
 import { styles } from '../styles/StyleSheet';
 import ImagBack from '../components/ImageBackground';
 import Rotas from "../components/Rotas";
+import Imagem from '../components/LoginImage';
+import Text from '../components/LoginText';
+import Input from '../components/LoginInput';
+import Button from '../components/LoginButton';
 
 export default function Login() {
 
@@ -9,8 +13,11 @@ return (
 <View style={{flex: 1}}>
 
 <Rotas />
-<ImagBack>
-
+<ImagBack >
+<Imagem/>
+<Text />
+<Input />
+<Button />
 </ImagBack>
 
 
